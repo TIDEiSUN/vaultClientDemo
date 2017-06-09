927fcde653cae4d20380e35cf5eb625758e582ec
Sum Ling <sumling@isuntv-hk.com>
2017-06-09 19:02:57 +0800

    Temp disable css for date picker

--- src/components/BankAccountPage.js
+++ src/components/BankAccountPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router';
 import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
+// import 'react-datepicker/dist/react-datepicker.css';
 import moment from 'moment';
 import AsyncButton from './common/AsyncButton';
 import ImageUpload from './common/ImageUpload';
